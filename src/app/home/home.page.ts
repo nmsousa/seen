406302f418fbe23afb10394aa449947c601c1384
nmsousa <wizard.night@gmail.com>
2023-02-06 02:13:17 +0100

    Added filter by SeenStatus on SeenStatus click

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,15 +5,23 @@ import {SeenStatus} from "../models/seen-status.enum";
 import {ModalController} from "@ionic/angular";
 import {MovieDialog} from "../movie-dialog/movie-dialog";
 
+export enum FilterType {
+    ALL = 'ALL',
+    TITLE = 'TITLE',
+    YEAR = 'YEAR',
+    SEEN_STATUS = 'SEEN_STATUS'
+}
+
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
     styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-
     originalDataSource: Movie[];
     dataSource: Movie[];
+    filterText: string;
+    filterType: FilterType = FilterType.ALL;
 
     constructor(private modalController: ModalController) {
     }
@@ -33,16 +41,43 @@ export class HomePage implements OnInit {
     }
 
     onSearchChange(event: any) {
-        const filterText = event.detail.value.toLowerCase();
-        this.filterData(filterText);
+        this.filterText = event.detail.value;
+        this.filterData();
     }
 
-    filterData(filterText: string) {
-        this.dataSource = this.originalDataSource.filter(movie => {
-            return movie.title.toLowerCase().includes(filterText) ||
-                movie.year.toString().includes(filterText) ||
-                movie.actors.toLowerCase().includes(filterText);
-        });
+    onFilterByYear(year: number) {
+        this.filterText = year.toString();
+        this.filterType = FilterType.YEAR;
+    }
+
+    onFilterByStatus(status: SeenStatus) {
+        this.filterText = status;
+        this.filterType = FilterType.SEEN_STATUS;
+    }
+
+    filterData() {
+        const filter = this.filterText.toLowerCase();
+        if (this.filterType === FilterType.ALL) {
+            this.dataSource = this.originalDataSource.filter(movie => {
+                return (movie.title && movie.title.toLowerCase().includes(filter)) ||
+                    (movie.year && movie.year.toString().includes(filter)) ||
+                    (movie.plot && movie.plot.toLowerCase().includes(filter)) ||
+                    (movie.director && movie.director.toLowerCase().includes(filter)) ||
+                    (movie.actors && movie.actors.toLowerCase().includes(filter)) ||
+                    (movie.seenStatus && movie.seenStatus.toLowerCase().includes(filter));
+            });
+        } else if (this.filterType === FilterType.YEAR) {
+            this.dataSource = this.originalDataSource.filter(movie => {
+                return movie.year && movie.year.toString().includes(filter);
+            });
+        } else if (this.filterType === FilterType.SEEN_STATUS) {
+            this.dataSource = this.originalDataSource.filter(movie => {
+                return movie.seenStatus && movie.seenStatus.toLowerCase().includes(filter);
+            });
+        }
+
+        // reset the filter type
+        this.filterType = FilterType.ALL;
     }
 
     async onAddMovie() {
@@ -54,14 +89,24 @@ export class HomePage implements OnInit {
         });
         modal.present();
 
-        const { data, role } = await modal.onWillDismiss();
+        const {data, role} = await modal.onWillDismiss();
         if (role === 'confirm') {
             this.originalDataSource.push(data);
             this.dataSource = [...this.originalDataSource];
         }
     }
 
-    onShowDetails(movie: Movie) {
+    onShowDetails(movie: Movie, accordion: any) {
         movie.isExpanded = !movie.isExpanded;
+        if (!accordion.value) {
+            accordion.value = 'details';
+        } else {
+            accordion.value = undefined;
+        }
+
+    }
+
+    onLabelClick() {
+
     }
 }
