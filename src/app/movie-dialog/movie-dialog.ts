a27c4c5130a8f3a6dec191165fbbf35aae5c5376
nmsousa <wizard.night@gmail.com>
2023-02-09 02:51:55 +0100

    - Fixed new movie, now shows chips and image in the movie list of the home page; - Simplified the way of getting the full image url

--- src/app/movie-dialog/movie-dialog.ts
+++ src/app/movie-dialog/movie-dialog.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ModalController} from "@ionic/angular";
 import {Movie} from "../models/movie.model";
 import {SeenStatus} from "../models/seen-status.enum";
@@ -9,21 +9,14 @@ import {MovieWsService} from "../services/movie-ws.service";
     templateUrl: './movie-dialog.html',
     styleUrls: ['./movie-dialog.scss'],
 })
-export class MovieDialog implements OnInit {
+export class MovieDialog {
     movie: Movie;
     SeenStatus = SeenStatus;
     filteredMovies: Movie[] = [];
     filterText: string;
     showMoviesList: boolean;
-    imagesBaseUrl: string;
 
     constructor(private modalController: ModalController, private movieWsService: MovieWsService) {
-    }
-
-    ngOnInit(): void {
-        this.movieWsService.getConfigurationImageBaseUrl().subscribe(imageBaseUrl => {
-            this.imagesBaseUrl = imageBaseUrl;
-        });
     }
 
     cancel() {
